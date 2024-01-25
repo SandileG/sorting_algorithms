50b0e86f2a3e3c79c053681cbbd27e3ece1b8703
SandileG <sandilegraham@gmail.com>
2024-01-25 05:00:25 -0800

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,7 +16,8 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		for (j = i +1; j < size; j++)
+
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
@@ -25,6 +26,7 @@ void selection_sort(int *array, size_t size)
 		if (min_idx != i)
 		{
 			int temp = array[i];
+
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
 
