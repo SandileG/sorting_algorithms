ec42a5586b6ff0e3b9a97ea22711251cc17d5dbe
SandileG <sandilegraham@gmail.com>
2024-01-25 04:54:22 -0800

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,29 +5,43 @@
  *               using the Bubble sort algorithm.
  * @array: The array of integers to be sorted.
  * @size: The size of the array.
+ *
+ * Description: This function implements the Bubble sort algorithm to
+ *              arrange the elements of the given array in ascending order.
+ *              It iteratively compares adjacent elements and swaps them if
+ *              they are in the wrong order. The process is repeated until
+ *              no more swaps are needed, indicating that the array is sorted.
+ *              The function also prints the array after each swap.
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
+	int swapped;
 	size_t i;
-	size_t j;
-	int temp;
-	
-	if (array == NULL || size < 2)
 
-	for (i = 0; i < size - 1; i++)
+	if (size < 2)
 	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				/* Swap array[j] and array[j + 1] */
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+		return;  /* No need to sort if size is less than 2 */
+	}
 
-				/* Print array after each swap */
+	do {
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				/* Swap elements */
+				int temp = array[i];
+
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = 1;
+
+				/* Print the array after each swap */
 				print_array(array, size);
 			}
 		}
-	}
+		size--;  /* Exclude the last element in subsequent passes */
+	} while (swapped);
 }
