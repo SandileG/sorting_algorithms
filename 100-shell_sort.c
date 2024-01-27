ab5a90dcf980500997dea742f947127df624deff
SandileG <122836823+SandileG@users.noreply.github.com>
2024-01-27 12:20:27 +0200

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,48 +1,48 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * exchange_values - Swap two integers in an array.
+ * @x: The first integer to exchange.
+ * @y: The second integer to exchange.
  */
-void swap_ints(int *a, int *b)
+void exchange_values(int *x, int *y)
 {
-	int tmp;
+    int temporary;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+    temporary = *x;
+    *x = *y;
+    *y = temporary;
 }
 
 /**
- * shell_sort - Sort an array of integers in ascending
- *              order using the shell sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
+ * knuth_shell_sort - Sort an array of integers in ascending
+ *                   order using the shell sort algorithm.
+ * @arr: An array of integers.
+ * @len: The length of the array.
  *
- * Description: Uses the Knuth interval sequence.
+ * Description: Utilizes the Knuth interval sequence.
  */
-void shell_sort(int *array, size_t size)
+void knuth_shell_sort(int *arr, size_t len)
 {
-	size_t gap, i, j;
+    size_t interval, i, j;
 
-	if (array == NULL || size < 2)
-		return;
+    if (arr == NULL || len < 2)
+        return;
 
-	for (gap = 1; gap < (size / 3);)
-		gap = gap * 3 + 1;
+    for (interval = 1; interval < (len / 3);)
+        interval = interval * 3 + 1;
 
-	for (; gap >= 1; gap /= 3)
-	{
-		for (i = gap; i < size; i++)
-		{
-			j = i;
-			while (j >= gap && array[j - gap] > array[j])
-			{
-				swap_ints(array + j, array + (j - gap));
-				j -= gap;
-			}
-		}
-		print_array(array, size);
-	}
+    for (; interval >= 1; interval /= 3)
+    {
+        for (i = interval; i < len; i++)
+        {
+            j = i;
+            while (j >= interval && arr[j - interval] > arr[j])
+            {
+                exchange_values(arr + j, arr + (j - interval));
+                j -= interval;
+            }
+        }
+        print_array(arr, len);
+    }
 }
