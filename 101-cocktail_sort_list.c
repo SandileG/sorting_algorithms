b44567b09df3801e8fa0f8be6f3e7e19a6dc815c
SandileG <sandilegraham@gmail.com>
2024-01-28 06:58:49 -0800

    cocktail

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -6,54 +6,56 @@
  * @tail: A pointer to the tail of the doubly-linked list.
  * @current: A pointer to the node to be swapped.
  */
-void swap_nodes_forward(listint_t **list, listint_t **tail, listint_t **current)
+void swap_nodes_forward(listint_t **list, listint_t **tail,
+			listint_t **current)
 {
-    listint_t *tmp = (*current)->next;
+	listint_t *tmp = (*current)->next;
 
-    if ((*current)->prev != NULL)
-        (*current)->prev->next = tmp;
-    else
-        *list = tmp;
+	if ((*current)->prev != NULL)
+		(*current)->prev->next = tmp;
+	else
+		*list = tmp;
 
-    tmp->prev = (*current)->prev;
-    (*current)->next = tmp->next;
+	tmp->prev = (*current)->prev;
+	(*current)->next = tmp->next;
 
-    if (tmp->next != NULL)
-        tmp->next->prev = *current;
-    else
-        *tail = *current;
+	if (tmp->next != NULL)
+		tmp->next->prev = *current;
+	else
+		*tail = *current;
 
-    (*current)->prev = tmp;
-    tmp->next = *current;
-    *current = tmp;
+	(*current)->prev = tmp;
+	tmp->next = *current;
+	*current = tmp;
 }
 
 /**
- * swap_nodes_backward - Swap a node with the previous node in a doubly-linked list.
+ * swap_nodes_backward - Swap node with previous node in doubly-linked list.
  * @list: A pointer to the head of the doubly-linked list.
  * @tail: A pointer to the tail of the doubly-linked list.
  * @current: A pointer to the node to be swapped.
  */
-void swap_nodes_backward(listint_t **list, listint_t **tail, listint_t **current)
+void swap_nodes_backward(listint_t **list, listint_t **tail,
+			listint_t **current)
 {
-    listint_t *tmp = (*current)->prev;
+	listint_t *tmp = (*current)->prev;
 
-    if ((*current)->next != NULL)
-        (*current)->next->prev = tmp;
-    else
-        *tail = tmp;
+	if ((*current)->next != NULL)
+		(*current)->next->prev = tmp;
+	else
+		*tail = tmp;
 
-    tmp->next = (*current)->next;
-    (*current)->prev = tmp->prev;
+	tmp->next = (*current)->next;
+	(*current)->prev = tmp->prev;
 
-    if (tmp->prev != NULL)
-        tmp->prev->next = *current;
-    else
-        *list = *current;
+	if (tmp->prev != NULL)
+		tmp->prev->next = *current;
+	else
+		*list = *current;
 
-    (*current)->next = tmp;
-    tmp->prev = *current;
-    *current = tmp;
+	(*current)->next = tmp;
+	tmp->prev = *current;
+	*current = tmp;
 }
 
 /**
@@ -63,37 +65,37 @@ void swap_nodes_backward(listint_t **list, listint_t **tail, listint_t **current
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *tail, *current;
-    int not_sorted = 0;
+	listint_t *tail, *current;
+	int not_sorted = 0;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    for (tail = *list; tail->next != NULL;)
-        tail = tail->next;
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
 
-    while (not_sorted == 0)
-    {
-        not_sorted = 1;
+	while (not_sorted == 0)
+	{
+		not_sorted = 1;
 
-        for (current = *list; current != tail; current = current->next)
-        {
-            if (current->n > current->next->n)
-            {
-                swap_nodes_forward(list, &tail, &current);
-                print_list((const listint_t *)*list);
-                not_sorted = 0;
-            }
-        }
+		for (current = *list; current != tail; current = current->next)
+		{
+			if (current->n > current->next->n)
+			{
+				swap_nodes_forward(list, &tail, &current);
+				print_list((const listint_t *)*list);
+				not_sorted = 0;
+			}
+		}
 
-        for (current = current->prev; current != *list; current = current->prev)
-        {
-            if (current->n < current->prev->n)
-            {
-                swap_nodes_backward(list, &tail, &current);
-                print_list((const listint_t *)*list);
-                not_sorted = 0;
-            }
-        }
-    }
+		for (current = current->prev; current != *list; current = current->prev)
+		{
+			if (current->n < current->prev->n)
+			{
+				swap_nodes_backward(list, &tail, &current);
+				print_list((const listint_t *)*list);
+				not_sorted = 0;
+			}
+		}
+	}
 }
