0243bc09bd5a2bb83ed94eeee01b1badecae6ef6
SandileG <sandilegraham@gmail.com>
2024-01-28 07:18:10 -0800

    cocktail

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,46 +7,47 @@
  */
 void counting_sort(int *array, size_t size)
 {
-        size_t max;
-        size_t i, j, index;
-        int *count_array;
+	size_t max;
+	size_t i, index;
+	int *count_array;
 
-        if (array == NULL || size < 2)
-                return;
+	if (array == NULL || size < 2)
+		return;
 
-        max = (size_t)array[0];
+	max = (size_t)array[0];
 
-        for (i = 1; i < size; i++)
-        {
-                if ((size_t)array[i] > max)
-                        max = (size_t)array[i];
-        }
+	for (i = 1; i < size; i++)
+	{
+		if ((size_t)array[i] > max)
+			max = (size_t)array[i];
+	}
 
-        count_array = malloc((max + 1) * sizeof(int));
+	count_array = malloc((max + 1) * sizeof(int));
 
-        if (count_array == NULL)
-                return;
+	if (count_array == NULL)
+		return;
 
-        for (i = 0; i <= max; i++)
-                count_array[i] = 0;
+	for (i = 0; i <= max; i++)
+		count_array[i] = 0;
 
-        for (i = 0; i < size; i++)
-                count_array[array[i]]++;
+	for (i = 0; i < size; i++)
+		count_array[array[i]]++;
 
-        print_array(count_array, max + 1);
+	print_array(count_array, max + 1);
 
-        index = 0;
+	index = 0;
 
-        for (i = 0; i <= max; i++)
-        {
-                for (j = 0; j < count_array[i]; j++)
-                {
-                        array[index] = (int)i;
-                        index++;
-                }
-        }
+	for (i = 0; i <= max; i++)
+	{
+		int j;
+		for (j = 0; j < count_array[i]; j++)
+		{
+			array[index] = (int)i;
+			index++;
+		}
+	}
 
-        print_array(count_array, max + 1);
+	print_array(count_array, max + 1);
 
-        free(count_array);
+	free(count_array);
 }
